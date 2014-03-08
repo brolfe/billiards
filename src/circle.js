810db8fdf0cfb78eba0597778891d52bbdbfe902
Bryant Rolfe <bryant.rolfe@gmail.com>
2014-03-08 09:37:27 -0500

    Field now updates the position of the circle if the circle exceeds the boundaries of the field.

--- src/circle.js
+++ src/circle.js
@@ -4,9 +4,6 @@ define([
 ], function( $ ){
     'use strict';
 
-    var X = 0; // constants for X and Y position in arrays
-    var Y = 1;
-
     $.widget('billiards.circle', {
         
         options: {
@@ -26,8 +23,8 @@ define([
             this.$element = this.element; // Use the $prefix naming convention
 
             this.$element.addClass('circle').css({
-                width: this.options.size + 'px',
-                height: this.options.size + 'px'
+                width: this.size() + 'px',
+                height: this.size() + 'px'
             });
 
             // Member vars
@@ -88,48 +85,18 @@ define([
         digest: function( scale ){
             scale = scale || 1;
 
-            var size = this.$field.field('size');
-
-            var leftX = this.options.px + this.options.vx / scale;
-            var topY  = this.options.py + this.options.vy / scale;
-
-            var rightX  = leftX + this.options.size;
-            var bottomY = topY + this.options.size;
-            
-            var vx = this.options.vx + this.options.ax / scale;
-            var vy = this.options.vy + this.options.ay / scale;
-
-            if ( rightX > size[ X ] ) {
-                rightX = size[ X ] - ( rightX % size[ X ] );
-                leftX = rightX - this.options.size;
-                vx = vx * -1;
-            }
-            
-            if ( leftX < 0 ) {
-                leftX = leftX * -1;
-                vx = vx * -1;
-            }
-
-            if ( bottomY > size[ Y ] ) {
-                bottomY = size[ Y ] - ( bottomY % size[ Y ] );
-                topY = bottomY - this.options.size;
-                vy = vy * -1;
-            }
-            
-            if ( topY < 0 ) {
-                topY = topY * -1;
-                vy = vy * -1;
-            }
-
-            // TODO: come up with something more concrete with digestFrequency, scale, etc.
-            // update position
-            this.options.px = leftX;
-            this.options.py = topY;
-            
-            this.options.vx = vx;
-            this.options.vy = vy;
+            // update postion / velocity
+            this.options.px += this.options.vx / scale;
+            this.options.py += this.options.vy / scale;
+    
+            this.options.vx += this.options.ax / scale;
+            this.options.vy += this.options.ay / scale;
 
             this._updatePosition();
+        },
+
+        size: function() {
+            return this.options.size;
         },
 
         setField: function( $field ){
--- src/field.js
+++ src/field.js
@@ -1,7 +1,8 @@
 define([
+    'underscore',
     'jquery',
     'jqui-widget'
-], function( $ ){
+], function( _, $ ){
     'use strict';
 
     var X = 0; // constants for X and Y position in arrays
@@ -35,12 +36,74 @@ define([
         // update each circle in the field
         digest: function( scale ){
 
+            // call digest on each circle
             _.each( this.circles, function( $c ){
-
-                // call digest on each circle
                 $c.circle( 'digest', scale );
-
             }, this );
+
+            // after all cicles have digested, adjust position/velocity accounting for boundary collisions
+            _.each( this.circles, function( $c ){
+                this._updateBoundaryCollision( $c, scale );
+            }, this );
+
+            // next, adjust position/velocity accounting for circle collisions
+
+        },
+
+        _updateBoundaryCollision: function( $circle, scale ) {
+            /* jshint maxstatements:35 */
+            scale = scale || 1;
+
+            var fieldSize = this.size();
+            var adjustmentMade = false; // do we need to update $circle?
+
+            // get the right, left, top, and bottom position of the square (circle, whatever)
+            var circlePos = $circle.circle('position');
+            var leftX = circlePos[ X ];
+            var topY  = circlePos[ Y ];
+
+            var rightX  = leftX + $circle.circle('size');
+            var bottomY = topY + $circle.circle('size');
+            
+            // get the velocity of the circle
+            var circleVelocity = $circle.circle('velocity');
+            var vx = circleVelocity[ X ];
+            var vy = circleVelocity[ Y ];
+
+            // if the right edge of the cicle exceeds the right edge of the field
+            if ( rightX > fieldSize[ X ] ) {
+                rightX = fieldSize[ X ] - ( rightX % fieldSize[ X ] );
+                leftX = rightX - $circle.circle('size');
+                vx = vx * -1;
+                adjustmentMade = true;
+            }
+            
+            // if the left edge of the cicle exceeds the left edge of the field
+            if ( leftX < 0 ) {
+                leftX = leftX * -1;
+                vx = vx * -1;
+                adjustmentMade = true;
+            }
+
+            // if the bottom edge of the cicle exceeds the bottom edge of the field
+            if ( bottomY > fieldSize[ Y ] ) {
+                bottomY = fieldSize[ Y ] - ( bottomY % fieldSize[ Y ] );
+                topY = bottomY - $circle.circle('size');
+                vy = vy * -1;
+                adjustmentMade = true;
+            }
+            
+            // if the top edge of the cicle exceeds the top edge of the field
+            if ( topY < 0 ) {
+                topY = topY * -1;
+                vy = vy * -1;
+                adjustmentMade = true;
+            }
+
+            if ( adjustmentMade ) {
+                $circle.circle( 'velocity', vx, vy );
+                $circle.circle( 'position', leftX, topY );
+            }
 
         },
 
